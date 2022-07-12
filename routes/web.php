8a8e3572361cd4394bf51d523250b113279f8e62
Yonny Cuesta <yonny.cuesta2017@unaula.edu.co>
2022-07-12 14:36:19 -0500

    Añadiendo Middleware a las rutas

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,17 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('/brands', [App\Http\Controllers\BrandController::class, 'index'])->name('brands.index');
+    Route::post('/brand/save', [App\Http\Controllers\BrandController::class, 'store'])->name('brand.store');
+    Route::get('/brand/delete/{id}', [App\Http\Controllers\BrandController::class, 'destroy']);
+    Route::get('/brand/edit/{id}', [App\Http\Controllers\BrandController::class, 'edit']);
+    Route::post('/brand/update/{id}', [App\Http\Controllers\BrandController::class, 'update'])->name('brand.update');
 
-Route::get('/brands', [App\Http\Controllers\BrandController::class, 'index'])->name('brands.index');
-Route::post('/brand/save', [App\Http\Controllers\BrandController::class, 'store'])->name('brand.store');
-Route::get('/brand/delete/{id}', [App\Http\Controllers\BrandController::class, 'destroy']);
-Route::get('/brand/edit/{id}', [App\Http\Controllers\BrandController::class, 'edit']);
-Route::post('/brand/update/{id}', [App\Http\Controllers\BrandController::class, 'update'])->name('brand.update');
-
-Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
-Route::post('/product/save', [App\Http\Controllers\ProductController::class, 'store'])->name('product.store');
-Route::get('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'destroy']);
-Route::get('/product/edit/{id}', [App\Http\Controllers\ProductController::class, 'edit']);
-Route::post('/product/update/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('product.update');
+    Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
+    Route::post('/product/save', [App\Http\Controllers\ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/delete/{id}', [App\Http\Controllers\ProductController::class, 'destroy']);
+    Route::get('/product/edit/{id}', [App\Http\Controllers\ProductController::class, 'edit']);
+    Route::post('/product/update/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('product.update');
+});
